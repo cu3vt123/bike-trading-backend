7e36dc9a6f42c073c6c87b19196e517744defde1
Hoangttx <xuanhoangtruong1302@gmail.com>
2026-03-23 20:53:40 +0700

    frontend Vite + Tailwind + axios

---
+++ bike-trading-frontend/src/Components/NavBars.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+
+function NavBars() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default NavBars
