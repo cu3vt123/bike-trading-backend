45d08a94f556964242e42414506ca07c42dc517b
Hoangttx <xuanhoangtruong1302@gmail.com>
2026-03-24 23:20:31 +0700

    add Nav links

--- bike-trading-frontend/src/Components/NavBars.tsx
+++ bike-trading-frontend/src/Components/NavBars.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../App.css";
 
@@ -9,6 +9,18 @@ function NavBars() {
                 <Navbar.Brand as={Link} to="/" className="navbar-custom-brand">
                     SPORTBIKE
                 </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="navbar-custom-nav mx-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/bikes">Bikes</Nav.Link>
+                        <Nav.Link as={Link} to="/featured">Featured</Nav.Link>
+                        <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
