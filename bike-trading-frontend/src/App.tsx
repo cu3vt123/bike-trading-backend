7d1f925b06943a72d7f7c4ac7236ee8d9882dfcb
Hoangttx <xuanhoangtruong1302@gmail.com>
2026-03-24 23:31:57 +0700

    render NavBars component and import bootstrap CSS

--- bike-trading-frontend/src/App.tsx
+++ bike-trading-frontend/src/App.tsx
@@ -1,19 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBars from "./Components/NavBars";
-import HomePage from "./Pages/HomePage";
-import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
     <div>
       <NavBars />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
     </div>
   );
-}
+};
 
 export default App;
