e7687c852e13f282bf03ac82845b3bfed947068c
Hoangttx <xuanhoangtruong1302@gmail.com>
2026-03-24 22:10:25 +0700

    create basic Navbar component structure

--- bike-trading-frontend/src/Components/NavBars.tsx
+++ bike-trading-frontend/src/Components/NavBars.tsx
@@ -1,21 +1,51 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Navbar, Nav, Container } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import "../App.css";
+// import { FaSearch, FaShoppingCart } from "react-icons/fa";
 
-const NavBars = () => {
+// function MyNavbar() {
+//     return (
+//         <Navbar className="navbar-custom" expand="lg">
+//             <Container fluid>
+//                 <Navbar.Brand
+//                     as={Link}
+//                     to="/"
+//                     className="navbar-custom-brand"
+//                 >
+//                     SPORTBIKE
+//                 </Navbar.Brand>
+
+//                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                 <Navbar.Collapse id="basic-navbar-nav">
+//                     <Nav className="navbar-custom-nav mx-auto">
+//                         <Nav.Link as={Link} to="/" >Home</Nav.Link>
+//                         <Nav.Link as={Link} to="/bikes">Bikes</Nav.Link>
+//                         <Nav.Link as={Link} to="/featured">Featured</Nav.Link>
+//                         <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+//                         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+//                         <Nav.Link as={Link} to="/login">Login</Nav.Link>
+//                     </Nav>
+
+//                     <div className="navbar-custom-search">
+//                         <input type="text" placeholder="Search..." />
+//                         <FaSearch className="icon" />
+//                         <FaShoppingCart className="icon" />
+//                     </div>
+//                 </Navbar.Collapse>
+//             </Container>
+//         </Navbar>
+//     );
+// }
+
+// export default MyNavbar;
+import React from 'react'
+
+function NavBars() {
     return (
-        <Navbar bg="light" variant="lightblue">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    ShopBike
-                </Navbar.Brand>
+        <div>
 
-                <Nav>
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
-};
+        </div>
+    )
+}
 
-export default NavBars;
+export default NavBars
