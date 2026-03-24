ebdaf41149c90a34919da66a51c1f39d81e054a4
Hoangttx <xuanhoangtruong1302@gmail.com>
2026-03-24 23:19:30 +0700

    add Navbar brand

--- bike-trading-frontend/src/Components/NavBars.tsx
+++ bike-trading-frontend/src/Components/NavBars.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Navbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function NavBars() {
     return (
-        <div>
-
-        </div>
+        <Navbar className="navbar-custom" expand="lg">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/" className="navbar-custom-brand">
+                    SPORTBIKE
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 }
 
